53d383c43bcd08eff721ab3d158a167a332ec0e7
KaterynaChernetska <vesnavved@gmail.com>
2023-02-06 14:55:54 +0200

    Add container

---
+++ react-app-team-4/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Container } from "./components/Container/Container";
+
+function App() {
+  return (
+    <>      
+      <Container><h2>Hello!</h2></Container>
+    </>
+  );
+}
+
+export default App;
---
+++ react-app-team-4/src/components/Container/Container.jsx
@@ -0,0 +1,5 @@
+export const Container = (props) => {
+    console.log(props);
+    return (<div>{props.children}</div>)
+
+}

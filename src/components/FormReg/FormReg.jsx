4f902d0a03ca7b9f573d0b6d7118ff3870a953b0
Iuliia Goriun <goriunyuliya@gmail.com>
2023-02-10 15:44:55 +0300

    Add submit function

--- src/components/FormReg/FormReg.jsx
+++ src/components/FormReg/FormReg.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { Component } from "react";
 
 export class FormReg extends Component {
@@ -5,23 +6,50 @@ export class FormReg extends Component {
     name: "",
     mail: "",
     password: "",
+    checkbox: false,
+    user: null,
   };
 
   handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({[name]: value.trim() });
+    const { name, checked, type, value } = e.target;
+
+    this.setState({[name]: type === 'checkbox' ? checked : value.trim() });
+
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, mail, password } = this.state;
+    const newUser = {
+    id: nanoid(),
+    name,
+    mail,
+    password,
+    }
+this.setState({
+  user: newUser,
+  name: "",
+  mail: "",
+  password: "",
+  checkbox: false,
+  
+})
+
+
+  }
+
   render() {
+    const { name, mail, password, user, checkbox } = this.state;
     return (
       <>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor=""></label>
           <input
             type="email"
             name="mail"
             onChange={this.handleChange}
-            value={this.state.mail}
+            value={mail}
+            required
           />
 
           <label htmlFor=""></label>
@@ -29,21 +57,29 @@ export class FormReg extends Component {
             type="text"
             name="name"
             onChange={this.handleChange}
-            value={this.state.name}
+            value={name}
+            required
           />
 
           <label htmlFor=""></label>
           <input
-            type="text"
+            type="password"
             name="password"
             onChange={this.handleChange}
-            value={"pass"}
+            value={password} 
+            required
           />
 
-          <input type="checkbox" />
+          <input type="checkbox" name="checkbox" onChange={this.handleChange} checked={checkbox}  required/>
 
           <button type="submit">Send info</button>
         </form>
+        {user &&
+        <> 
+        <p>name:{user.name}</p>
+        <p>mail:{user.mail}</p>
+        <p>password:{user.password}</p>
+        </>}
       </>
     );
   }

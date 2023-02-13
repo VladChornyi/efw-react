256173d8ce6565afb4ed8991477b2f01d7aaf8c1
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-02-13 15:49:34 +0200

    Update Tabs.jsx

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -3,11 +3,26 @@ import React, { Component } from "react";
 import "./Tabs.css";
 import tabs from "../../data/tabs.json";
 
+const LOCAL_KEY = "index";
+
 export default class Tabs extends Component {
   state = {
     ind: 0,
   };
+
+  componentDidMount() {
+    const localData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+    console.log(localData, this.state.ind);
+    if (localData === null) {return}
+    this.setState({ind: localData,});
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    localStorage.setItem(LOCAL_KEY, this.state.ind);
+  }
+
   handleClick = (actInd) => this.setState({ ind: actInd });
+
   render() {
     return (
       <div class="tabs">

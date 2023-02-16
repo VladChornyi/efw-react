8857fce9f40ecc9561cbc23cc6774c604e0af64d
Maryna <yasinskamarina@gmail.com>
2023-02-16 15:01:28 +0200

    fq,formreg

--- src/components/AccordionItem/AccordionItems.jsx
+++ src/components/AccordionItem/AccordionItems.jsx
@@ -1,49 +1,51 @@
-import React, { Component } from "react";
+import React, {  useState } from "react";
 import "./AccordionItems.css";
-export class AccordionItem extends Component {
-  state = { isOpen: false };
-  handleToggleAccordion = () => {
-    this.setState((prevState) => {
-      return {
-        isOpen: !prevState.isOpen,
-      };
-    });
-  };
-  render() {
-    return (
-      <div className="container">
-        <div className="accordion">
-          <div className="accordion-item">
-            <p
-              className={`title ${this.state.isOpen ? "is-expanded" : ""}`}
-              onClick={this.handleToggleAccordion}
-            >
-              First Question
-            </p>
+export function AccordionItem() {
+  // state = { isOpen: false };
+  const [isOpen, setIsOpen] = useState(false);
 
-            <div
-              className={`content ${this.state.isOpen && "is-expanded"}`}
-              onClick={this.handleToggleAccordion}
-            >
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-                eu interdum diam. Donec interdum porttitor risus non bibendum.
-                Maecenas sollicitudin eros in quam imperdiet placerat. Cras
-                justo purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec
-                dignissim arcu nec elit faucibus condimentum. Donec facilisis
-                consectetur enim sit amet varius. Pellentesque justo dui,
-                sodales quis luctus a, iaculis eget mauris. Aliquam dapibus,
-                ante quis fringilla feugiat, mauris risus condimentum massa, at
-                elementum libero quam ac ligula. Pellentesque at rhoncus dolor.
-                Duis porttitor nibh ut lobortis aliquam. Nullam eu dolor
-                venenatis mauris placerat tristique eget id dolor. Quisque
-                blandit adipiscing erat vitae dapibus. Nulla aliquam magna nec
-                elementum tincidunt.
-              </p>
-            </div>
+  const handleToggleAccordion = () => {
+    setIsOpen((prevState) => !prevState);
+
+    // this.setState((prevState) => {
+    //   return {
+    //     isOpen: !prevState.isOpen,
+    //   };
+    // });
+  };
+
+  return (
+    <div className="container">
+      <div className="accordion">
+        <div className="accordion-item">
+          <p
+            className={`title ${isOpen ? "is-expanded" : ""}`}
+            onClick={handleToggleAccordion}
+          >
+            First Question
+          </p>
+
+          <div
+            className={`content ${isOpen && "is-expanded"}`}
+            onClick={handleToggleAccordion}
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu
+              interdum diam. Donec interdum porttitor risus non bibendum.
+              Maecenas sollicitudin eros in quam imperdiet placerat. Cras justo
+              purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec dignissim
+              arcu nec elit faucibus condimentum. Donec facilisis consectetur
+              enim sit amet varius. Pellentesque justo dui, sodales quis luctus
+              a, iaculis eget mauris. Aliquam dapibus, ante quis fringilla
+              feugiat, mauris risus condimentum massa, at elementum libero quam
+              ac ligula. Pellentesque at rhoncus dolor. Duis porttitor nibh ut
+              lobortis aliquam. Nullam eu dolor venenatis mauris placerat
+              tristique eget id dolor. Quisque blandit adipiscing erat vitae
+              dapibus. Nulla aliquam magna nec elementum tincidunt.
+            </p>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
